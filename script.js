84f1d0a817508c2253f52c77c81ecd153f8e1a3c
Debnath <166276275+Debnath225@users.noreply.github.com>
2024-11-15 23:03:26 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const image_input = document.querySelector("#image_input");
+var uploaded_image = "";
+
+image_input.addEventListener("change", function(){
+const reader = new FileReader();
+reader.addEventListener("load", () => {
+  unloaded_image = reader.result;
+  document.quearySelector("#display_image").style.backgroundImage = `url(${upload_image})`
+
+});
+  reader.readAsDataURL(this.files[0])
+
+})
