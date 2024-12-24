900a2579be4fcdea1efadfb41ecaab4797ea849c
Debnath <166276275+Debnath225@users.noreply.github.com>
2024-12-24 17:07:04 +0530

    Update script.js
    Ok

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ function drop(event) {
   let data = event.dataTransfer.getData("text");
   event.target.appendChild(document.getElementById(data));
 }
+function clickCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.clickcount) {
+      sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+    } else {
+      sessionStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "You have like the photo " + sessionStorage.clickcount + " time(s) in this session.";
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+  }
+}
